efa1c76bb46e3b762a72ff90f222b18caca07718
takizawam <tkzw.g.jm@gmail.com>
2020-02-04 12:11:47 +0900

    jsのpauseを削除

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ var audioContext //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
-var pauseButton = document.getElementById("pauseButton");
+// Deleted by Takizawa
+// var pauseButton = document.getElementById("pauseButton");
 
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
-pauseButton.addEventListener("click", pauseRecording);
+// Deleted by Takizawa
+// pauseButton.addEventListener("click", pauseRecording);
 
+
+// startRecording関数
 function startRecording() {
 	console.log("recordButton clicked");
 
@@ -28,34 +32,21 @@ function startRecording() {
 
     var constraints = { audio: true, video:false }
 
-		// 滝沢により追加 Using Audio Constraints With getUserMedia()
-		// var constraints = {
-		//     audio: {
-		//         sampleRate: 48000,
-		//         channelCount: 2,
-		//         volume: 1.0
-		//     },
-		//     video: true
-		// }
-		// navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		//     /* use the stream */
-		// }).catch(function(err) {
-		//     /* handle the error */
-		// });
-
  	/*
     	Disable the record button until we get a success or fail from getUserMedia()
 	*/
 
 	recordButton.disabled = true;
 	stopButton.disabled = false;
-	pauseButton.disabled = false
+	// Removed by Takizawa
+	// pauseButton.disabled = false;
 
 	/*
     	We're using the standard promise based getUserMedia()
     	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 	*/
 
+		//　これで音声録音許可され、開始される
 	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
 		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
@@ -66,58 +57,61 @@ function startRecording() {
 		*/
 		audioContext = new AudioContext();
 
-		//update the format
-		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+		//サンプリング周波数を表示
+		// document.getElementById("formats").innerHTML="サンプリング周波数 "+audioContext.sampleRate/1000+"kHz"
+		document.getElementById("formats").innerHTML=audioContext.sampleRate/1000+"kHz"
 
 		/*  assign to gumStream for later use  */
 		gumStream = stream;
-
 		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
-
 		/*
 			Create the Recorder object and configure to record mono sound (1 channel)
 			Recording 2 channels  will double the file size
 		*/
+		// 2 channel録音にすることも可能
 		rec = new Recorder(input,{numChannels:1})
-
 		//start the recording process
 		rec.record()
-
 		console.log("Recording started");
-
 	}).catch(function(err) {
 	  	//enable the record button if getUserMedia() fails
     	recordButton.disabled = false;
     	stopButton.disabled = true;
-    	pauseButton.disabled = true
+			// Deleted by Takizawa
+    	// pauseButton.disabled = true
 	});
 }
+// // Deleted by Takizawa
+// function pauseRecording(){
+// 	console.log("pauseButton clicked rec.recording=",rec.recording );
+// 	if (rec.recording){
+// 		//pause
+// 		rec.stop();
+// 		pauseButton.innerHTML="Resume";
+// 	}else{
+// 		//resume
+// 		rec.record()
+// 		pauseButton.innerHTML="Pause";
+//
+// 	}
+// }
 
-function pauseRecording(){
-	console.log("pauseButton clicked rec.recording=",rec.recording );
-	if (rec.recording){
-		//pause
-		rec.stop();
-		pauseButton.innerHTML="Resume";
-	}else{
-		//resume
-		rec.record()
-		pauseButton.innerHTML="Pause";
 
-	}
-}
 
+// stopRecording関数
 function stopRecording() {
 	console.log("stopButton clicked");
 
 	//disable the stop button, enable the record too allow for new recordings
 	stopButton.disabled = true;
 	recordButton.disabled = false;
-	pauseButton.disabled = true;
+	// // Removed by Takizawa
+	// pauseButton.disabled = true;
 
-	//reset button just in case the recording is stopped while paused
-	pauseButton.innerHTML="Pause";
+	// // Removed by Takizawa
+	// //reset button just in case the recording is stopped while paused
+	// pauseButton.innerHTML="Pause";
 
 	//tell the recorder to stop the recording
 	rec.stop();
@@ -129,6 +123,9 @@ function stopRecording() {
 	rec.exportWAV(createDownloadLink);
 }
 
+
+
+// createDownloadLink(blob)関数
 function createDownloadLink(blob) {
 
 	var url = URL.createObjectURL(blob);
@@ -147,7 +144,8 @@ function createDownloadLink(blob) {
 	//save to disk link
 	link.href = url;
 	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "保存";
+  // 保存を削除 Deleted by Takizawa
+	// link.innerHTML = "保存";
 
 	//add the new audio element to li
 	li.appendChild(au);
@@ -158,10 +156,11 @@ function createDownloadLink(blob) {
 	//add the save to disk link to li
 	li.appendChild(link);
 
-	//upload link
+	//upload link(wavファイルをXHRを用いてアップロードする)
 	var upload = document.createElement('a');
 	upload.href="#";
-	upload.innerHTML = "Upload";
+	upload.innerHTML = "アップロード";
+	// アップロードボタンを押したらアップロード
 	upload.addEventListener("click", function(event){
 		  var xhr=new XMLHttpRequest();
 		  xhr.onload=function(e) {
@@ -178,7 +177,6 @@ function createDownloadLink(blob) {
 			// xhr.open("POST","http://localhost:5000/webapp",true);
 			// /Applications/MAMP/htdocs ←なんでここがlocalhost/upload.phpなの？
 		  xhr.send(fd);
-			console.log(xhr.response);
 
 	})
 	li.appendChild(document.createTextNode (" "))//add a space in between
